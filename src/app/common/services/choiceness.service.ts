c5c4ffbb12715503f0e9e81859310a4d327f3ddc
Jet Xu <jet.xu@agileact.com>
2018-02-26 14:02:17 +0800

    对接精选页面分类的内容API

--- src/app/common/services/choiceness.service.ts
+++ src/app/common/services/choiceness.service.ts
@@ -9,7 +9,29 @@ export class ChoicenessService {
 
   }
 
+  /**
+   * 获取精选页面轮播图
+   * url: 'http://i.qingting.fm/capi/neo-recommend/banner'
+   */
   public getChiconessBanners(): Observable<any> {
     return this.hc.get('http://i.qingting.fm/capi/neo-recommend/banner');
   }
+
+  /**
+   * 获取精选页面属性
+   * url: 'http://i.qingting.fm/capi/neo-recommend/attrs'
+   */
+  public getChiconessAttrs(): Observable<any> {
+    return this.hc.get('http://i.qingting.fm/capi/neo-recommend/attrs');
+  }
+
+  /**
+   * 获取精选页面属性
+   * @param attrs 属性数组
+   * url: 'http://recpage.c.qingting.fm/v2/hotpage/category/${attr}'
+   */
+  public getChiconessCategory(attrs: string): Observable<any> {
+    return this.hc.get(`http://recpage.c.qingting.fm/v2/hotpage/category/${attrs}`);
+  }
+
 }
--- src/app/fm-modules/choiceness/choiceness.component.ts
+++ src/app/fm-modules/choiceness/choiceness.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { ChoicenessService } from '../../common/services/choiceness.service';
-
+import * as _ from 'lodash';
 @Component({
     selector: 'app-choiceness',
     templateUrl: './choiceness.component.html',
@@ -9,19 +9,68 @@ import { ChoicenessService } from '../../common/services/choiceness.service';
 })
 export class ChoicenessComponent implements OnInit {
     array = [1];
-    public items: Array<any>;
+    public banners: Array<any>;
+    public attrs: Array<any>;
+    public categories: Array<any>;
 
     constructor(
         private choicenessService: ChoicenessService
     ) {}
-    
+
     ngOnInit() {
         setTimeout(_ => {
             this.array = [1, 2, 3, 4];
         }, 500);
 
-        this.choicenessService.getChiconessBanners().subscribe((items) => {
-            this.items = items['data'];
+        // 获取轮播图
+        this.choicenessService.getChiconessBanners().subscribe((banners) => {
+            this.banners = banners['data'];
+        });
+
+        // 获取属性
+        this.getChiconessAttrs();
+
+
+    }
+
+    getChiconessAttrs() {
+        this.choicenessService.getChiconessAttrs().subscribe((attrs) => {
+            this.attrs = attrs['data'];
+            console.log(this.attrs);
+            let arr = [];
+            this.attrs.forEach(attr => {
+                arr.push(attr.id);
+            });
+            console.log(arr);
+            arr.sort(this.sortNumber);
+            console.log(arr);
+
+            let str = arr.join('_');
+            console.log(str);
+
+            // 获取分类内容
+            this.getChiconessCategory(str);
+        });
+    }
+
+    // 数组元素是数字进行排序时，必须调用一个排序函数
+    sortNumber(a, b) {
+        return a - b;
+    }
+
+    getChiconessCategory(str: string) {
+        this.choicenessService.getChiconessCategory(str).subscribe((attrs) => {
+            this.categories = _.valuesIn(attrs['data']);
+            // console.log(_.toPairsIn(this.categories)); // 返回键值对的数组
+            console.log(this.categories); // 返回对象属性的值的数组
+            this.categories.forEach(cate => {
+                this.attrs.forEach(attr => {
+                   if (cate.more.category_id === String(attr.id)) {
+                       cate.attr = attr;
+                   }
+                });
+            });
+            console.log(this.categories);
         });
     }
 }
